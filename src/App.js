5ca76ca5bb11054376ca9b92b91183579380d48e
naima <naimaosman203@gmail.com>
2022-05-21 12:12:22 +0100

    added Balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header'
+import { Balance } from './components/Balance'
 
 function App() {
   return (
     <div className="App">
       < Header />
+      <div className='container'>
+      <Balance />
+      </div>
     </div>
   );
 }
